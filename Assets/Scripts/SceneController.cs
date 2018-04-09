42e7c6253c2227808d6267de29906e5a6fd5923b
dorgrin6 <dorgrin6@gmail.com>
2018-04-09 13:39:53 +0300

    moved thirdparty to dedicated foler. changed SceneManager to Scene controller

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+namespace Physikef
+{
+    public class SceneController : MonoBehaviour
+    {
+        [SerializeField] float gravity = 30f;
+        [SerializeField] float massMult = 1f;
+        [SerializeField] float dragMult;
+        [SerializeField] float angularDragMult;
+
+        // Use this for initialization
+        void Start()
+        {
+            //Physics.gravity = new Vector3(0, -gravity, 0);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
