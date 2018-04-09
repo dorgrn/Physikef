8e7bf42caaa763925db16af6517241ed5c67b1d3
Idan Goor <idangoo@mta.ac.il>
2018-04-09 14:54:55 +0300

    GravityProperty
    Co-Authored-By: inarasempire <inarasempire@users.noreply.github.com>

---
+++ Assets/Scripts/PropertyController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PropertyController : MonoBehaviour
+{
+    public Button ButtonUp;
+
+    public Button ButtonDown;
+
+    public InputField Field;
+
+    public float InitVal;
+
+    private float m_Value;
+
+    public int Precision;
+
+    public float Offset;
+
+    public float MaxVal;
+
+    public float MinVal;
+
+    public float Value
+    {
+        get
+        {
+            return m_Value;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Value = InitVal;
+        ButtonUp.onClick.AddListener(OnButtonUpClick);
+        ButtonDown.onClick.AddListener(OnButtonDownClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Field.text = m_Value.ToString("0.##");
+    }
+
+    void OnButtonUpClick()
+    {
+        if (m_Value < MaxVal)
+        {
+            m_Value += Offset;
+        }
+    }
+    
+    void OnButtonDownClick()
+    {
+        if (m_Value > MinVal )
+        {
+            m_Value -= Offset;
+        }
+    }
+
+}
