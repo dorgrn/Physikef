f0af0863b726bdd7e90d6ad152a7cccd71ef326a
dorgrin6 <dorgrin6@gmail.com>
2018-08-19 20:31:08 +0300

    fixed pendulum scene

---
+++ Assets/Scripts/GameScenes/Penudulum/ColliderDetector.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace GameScenes.Penudulum
+{
+    public class ColliderDetector : MonoBehaviour
+    {
+        private Stopwatch collisionTimer;
+        private int collisionNumber = 0;
+
+        private void Start()
+        {
+            collisionTimer = Stopwatch.StartNew();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Bob"))
+            {
+                collisionNumber++;
+                Debug.Log("Hit number:" + collisionNumber);
+
+                if (collisionNumber % 2 == 0) // is oscillation of bob
+                {
+                    Debug.Log("timer is: " + collisionTimer.ElapsedMilliseconds);
+                    collisionTimer = Stopwatch.StartNew();
+                }
+            }
+            else
+            {
+                Debug.Log("problem!");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameScenes/Penudulum/PendulumSimleImp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace GameScenes.Penudulum
+{
+    public class PendulumSimleImp : MonoBehaviour
+    {
+        public Transform pivot;
+        public float speed = 0.5f;
+        private Vector3 v3Pivot; // Pivot in screen space
+        private bool dragging = false;
+        private float startAngle = 0.0f;
+        private float endAngle = -180.0f;
+        private float fTimer = 0.0f;
+        private float angleDiff;
+        private Vector3 v3T = Vector3.zero;
+        private float angle;
+
+        void Start()
+        {
+            v3Pivot = Camera.main.WorldToScreenPoint(pivot.position);
+        }
+
+        void Update()
+        {
+            float f = (Mathf.Sin(fTimer * speed - Mathf.PI / 2.0f) + 1.0f) / 2.0f;
+
+            v3T.Set(0.0f, 0.0f, Mathf.Lerp(startAngle, endAngle, f));
+            pivot.eulerAngles = v3T;
+            fTimer += Time.deltaTime;
+        }
+    }
+}
