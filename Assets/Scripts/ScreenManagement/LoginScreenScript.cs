4ba8e5ed9cef325a6011a4c3ee392f13be8ff2e3
olga abir <olga_abir@yahoo.com>
2018-08-25 11:30:52 +0300

    create new account redirects to register screen

---
+++ Assets/Scripts/ScreenManagement/LoginScreenScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginScreenScript : MonoBehaviour {
+
+    public void CreateNewAccount_Click()
+    {
+        SceneManager.LoadScene("RegisterScreen");
+    }
+}
